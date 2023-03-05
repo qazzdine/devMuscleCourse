3a091b0f6f071c1f575f436c5d3cf1884956a190
Azzeddine Qarouach <azzeddine.qarouach@orange.com>
2023-03-05 10:52:47 +0100

    firstCommit

---
+++ src/Controller/AqaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AqaController extends AbstractController
+{
+    #[Route('/aqa', name: 'app_aqa')]
+    public function index(): Response
+    {
+        return $this->render('aqa/index.html.twig', [
+            'controller_name' => 'AqaController',
+        ]);
+    }
+}
